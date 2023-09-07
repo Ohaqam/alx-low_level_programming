7fef6ff0ad3c7c4b29d595882e53d78a8e270ce5
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-07 15:01:47 -0700

    Done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-ls
+#include <stdio.h>
+
+/**
+ * main - Prints with printf
+ *
+ *Return: Always 0 (Success) 
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
