9456610841743e168c2848bbcc4964e894fabb36
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-07 15:25:24 -0700

    Files created

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-ls
+#include <stdio.h>
+
+/**
+* main - Prints with fwrite 
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
