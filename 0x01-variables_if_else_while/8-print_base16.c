501bcc3a07c5b010d0b34163e376663583306cc6
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-08 16:48:35 -0700

    Done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,19 @@
 */
 
 int main(void)
-char n = '0';
-while (n <= 'f')
+
+  int n = 0;
+char c = 'a';
+
+while (n < 10)
 {
-putchar((n % 16) + '0');
+putchar((n % 10) + '0');
 n++;
+}
+while (c <=f)
+{
+putchar(c);
+c++
 }
 putchar('\n');
 return (0);
