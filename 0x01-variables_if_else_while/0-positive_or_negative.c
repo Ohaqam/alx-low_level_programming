2b3fc8ba022142066cebdadd4f1da114ab01c218
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-08 13:07:44 -0700

    Done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-  printf("%d\n,is positive",n);
+printf("%d,is positive",n);
 }
 if (n == 0)
 {
-printf("is zero");
+printf("%d","is zero",n);
 }
 if (n < 0)
 {
-printf("is negative");
+printf("%d","is negative",n);
 }
 return (0);
 }
