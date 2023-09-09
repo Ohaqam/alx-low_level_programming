b6b391157fbda5ca016e4c306fcc282a866db160
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-08 17:11:50 -0700

    Done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,4 +6,16 @@
 * Return: Always 0 (Success)
 */
 
-
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+putchar((n % 10) + '0');
+putchar(',');
+putchar(' ');
+n++;
+}
+putchar('\n');
+return (0);
+}
