ed22c8bdcafa8fdc0b2095143f8c82ba7246d643
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-10 06:37:03 -0700

    Done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,4 +6,29 @@
 * Return: Always 0 (Success)
 */
 
-
+int main(void)
+{
+int n1;
+int n2;
+for (n1 = 0 ; n1 < 100 ; n1++)
+{
+for (n2 = n1 ; n2 < 100 ; n2++)
+{
+if (n2 != n1)
+{
+putchar((n1 /10) + 48);
+putchar((n1 % 10) + 48);
+putchar(' ');
+putchar((n2 / 10) + 48);
+putchar((n2 % 10) + 48);
+if (n1 != 98 || n2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
