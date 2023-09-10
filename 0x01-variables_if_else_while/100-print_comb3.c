4b6fe4805a6f38a23daea68e9b5c19fa5a1c4d78
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-10 05:22:54 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,7 @@ for (n1 = 0 ; n1 < 10 ; n1++)
 {
 for (n2 = n1 ; n2 < 10 ; n2++)
 {
-if(n1 != n2 && n1 < n2)
+if (n1 != n2 && n1 < n2)
 {
 putchar(n1 + 48);
 putchar(n2 + 48);
@@ -24,7 +24,7 @@ putchar(',');
 putchar(' ');
 }
 }
-}   
+} 
 }
 putchar('\n');
 return (0);
