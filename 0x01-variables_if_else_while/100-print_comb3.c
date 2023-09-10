991dfeda544aceb9c1fedbb0927f1ee18a3fff59
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-10 05:21:32 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,15 +12,19 @@ int n1;
 int n2;
 for (n1 = 0 ; n1 < 10 ; n1++)
 {
-for (n2 = 0 ; n2 < 10 ; n2++)
+for (n2 = n1 ; n2 < 10 ; n2++)
 {
-putchar((n1 % 10) + '0');
-putchar((n2 % 10) + '0');
-if (n1 == 8 && n2 == 9)
-break;
+if(n1 != n2 && n1 < n2)
+{
+putchar(n1 + 48);
+putchar(n2 + 48);
+if (n1 + n2 != 17)
+{
 putchar(',');
 putchar(' ');
 }
+}
+}   
 }
 putchar('\n');
 return (0);
