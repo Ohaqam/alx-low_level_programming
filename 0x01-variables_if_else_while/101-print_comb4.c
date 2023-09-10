0d6d7da70d17b804985f394dee2e6f3311835835
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-10 05:50:54 -0700

    Done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
 * Return: Always 0 (Success)
 */
 
-int main()
+int main(void)
 {
 int n1;
 int n2;
@@ -17,12 +17,12 @@ for (n2 = n1 ; n2 < 10 ; n2++)
 {
 for (n3 = n2 ; n3 < 10 ; n3++)
 {
-if(n1 != n2 && n2 != n3 && n1 < n2 && n2 < n3)
+if (n1 != n2 && n2 != n3 && n1 < n2 && n2 < n3)
 {
 putchar(n1 + 48);
 putchar(n2 + 48);
 putchar(n3 + 48);
-if (n1 + n2 + n3 != 24 )
+if (n1 + n2 + n3 != 24)
 {
 putchar(',');
 putchar(' ');
@@ -32,5 +32,5 @@ putchar(' ');
 }
 }
 putchar('\n');
-return 0;
+return (0);
 }
