26bd2d51b60811a4f3aaa2a420c6ed53ef54bdb3
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-12 15:57:10 -0700

    Done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,6 +4,7 @@
 * main - check the code
 * @c:checks for alphabetic character.
 * Returns 1 if c is a letter, lowercase or uppercase, Returns 0 otherwise
+* Return: Always 0.
 */
 
 int _isalpha(int c)
