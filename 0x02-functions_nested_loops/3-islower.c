44eb939796423f3fe5a4090d2575b3f39f6a38ef
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-12 14:36:09 -0700

    Done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,9 +6,13 @@
 * Return: Always 0.
 */
 
-int main(void)
+int _islower(int c)
 {
-print_alphabet();
-return (0);
+int r;
+if (r >= 'a' && r <= 'z')
+{
+_putchar('1');
 }
-
+else
+_putchar('0');
+}
