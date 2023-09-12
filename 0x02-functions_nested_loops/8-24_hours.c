6158b502b873c9bb6adce1ca4c72731f3a2ad77a
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-12 16:53:16 -0700

    Done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,19 @@
 * Return: Always 0.
 */
 
-int main(void)
+void jack_bauer(void)
 {
-print_alphabet();
-return (0);
+int min , hr;
+for (hr = 0; hr <=23; hr++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((hr / 10) + 48);
+_putchar((hr % 10) + 48);
+_putchar(:);
+_putchar((min / 10) + 48);
+_putchar((min % 10) + 48);
+_putchar('\n');
 }
+     
 
