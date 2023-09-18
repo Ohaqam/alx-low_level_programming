37a4621f7f83234e3374013838d3c9c1693d1da5
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-18 15:58:44 -0700

    Done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@ void print_rev(char *s)
 int len = 0, index = 0;
 len = *s;
 
-for (index = len - 1; index >= 0; index--)
+for (index = len - 1; index <= sizeof(len)/sizeof(len[0]) - 1 ; index--)
 _putchar(s[index]);
 
 _putchar('\n');
