dc53835f2326f5633ca7f3681a0089c80e48d91f
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-18 13:44:30 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*reset_to_98 - function that takes a pointer to an int as parameter
+*@n: tested character
+*Return: returns 0
+*/
+
+void swap_int(int *a, int *b)
+{
+*a = 42
+*b = 98
+}
