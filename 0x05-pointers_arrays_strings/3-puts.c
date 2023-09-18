c960e99309475efe80da994ce139be62ad71654f
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-18 15:34:28 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_puts - function that prints a string
+* @newstr: string to print
+* On success: return the number of characters printed
+*/
+
+void _puts(char *str)
+{
+newstr = *str;
+while (newstr)
+_putchar(newstr++);
+_putchar('\n');
+}
