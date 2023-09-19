7b464ea628c1a829237253833b51f346b869e691
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-09-18 17:11:26 -0700

    Done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,6 +7,7 @@
 */
 
 void rev_string(char *s)
+{
 int l, i;
 char temp;
 for (l = 0; s[l] != '\0'; l++)
@@ -16,4 +17,5 @@ for (i = 0; i < l / 2; i++)
 temp = s[i];
 s[i] = s[l - 1 - i];
 s[l - 1 -i] = temp;
+}
 }
