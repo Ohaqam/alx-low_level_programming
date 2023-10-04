4532a347cb4c0cd68cce92c15da48ad6dcf3a3a8
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-10-04 16:28:14 -0700

    Done

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* free_grid - function that frees a 2 dimensional grid 
+* free_grid - function that frees a 2 dimensional grid
 * @grid:      Parameter one.
 * @height:    Parameter two.
 * Return:     Returns nothing.
