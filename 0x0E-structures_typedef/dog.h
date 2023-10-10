e7a3726573c7f40e3c0cd90f2029315c91bd2ca6
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-10-10 13:02:23 -0700

    Done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,4 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* struct dog - New dog type
+* @name: Corresponds to Parameter.
+* @age:  Corresponds to Parameter.
+* @owner:Corresponds to Parameter.
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+
 #endif
