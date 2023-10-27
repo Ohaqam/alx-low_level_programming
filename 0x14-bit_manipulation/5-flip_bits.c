10bf425cdcc11313249dcb6bdae62fcf5af1a139
Omar Abdulmoneim <ohaq.2004@gmail.com>
2023-10-26 17:27:02 -0700

    Done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* flip_bits - function that returns the number of 
+* flip_bits - function that returns the number of
 * bits you would need to flip to get from one number to another.
 * @n:         number one.
 * @m:         number two.
